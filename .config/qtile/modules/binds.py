066b2c64eb2d89f658d704f2fa210323fe0b7473
SoyAlejandroCalixto <soyalejandrocalixto@gmail.com>
2024-06-12 06:02:15 +0100

    fixes

--- .config/qtile/modules/binds.py
+++ .config/qtile/modules/binds.py
@@ -4,8 +4,8 @@ from libqtile.config import Key, Drag, Click
 from libqtile.lazy import lazy
 from .consts import groups, default_terminal
 
-super =	"mod4"
-alt =	"mod1"
+super = "mod4"
+alt = "mod1"
 left_click = 'Button1'
 wheel_click = 'Button2'
 right_click = 'Button3'
@@ -14,9 +14,12 @@ right_click = 'Button3'
 
 keys_binds = [
     # move focus window
-    Key([super, "control"], "j", lazy.layout.shuffle_left(), desc="Move window to the left"),
-    Key([super, "control"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
-    Key([super, "control"], "k", lazy.layout.shuffle_down(), desc="Move window down"),
+    Key([super, "control"], "j", lazy.layout.shuffle_left(),
+        desc="Move window to the left"),
+    Key([super, "control"], "l", lazy.layout.shuffle_right(),
+        desc="Move window to the right"),
+    Key([super, "control"], "k", lazy.layout.shuffle_down(),
+        desc="Move window down"),
     Key([super, "control"], "i", lazy.layout.shuffle_up(), desc="Move window up"),
 
     # change focus window size
@@ -24,13 +27,13 @@ keys_binds = [
     Key([super], "l", lazy.layout.grow_right()),
     Key([super], "k", lazy.layout.grow_down()),
     Key([super], "i", lazy.layout.grow_up()),
-    Key([super], "r", lazy.layout.normalize()), # reset windows size
-    
+    Key([super], "r", lazy.layout.normalize()),  # reset windows size
+
     # manage windows
     Key([super], "w", lazy.window.kill()),
     Key([super], "f", lazy.window.toggle_fullscreen()),
-    Key([super], "t", lazy.window.toggle_floating()), # toggle floating window
-    Key([alt], "space", lazy.layout.next()), # move focus to next windows
+    Key([super], "t", lazy.window.toggle_floating()),  # toggle floating window
+    Key([alt], "space", lazy.layout.next()),  # move focus to next windows
 
     # utils (screenshots, volume, emotes...)
     Key([super], "p", lazy.spawn('amixer -c 0 sset Master 1+ unmute')),
@@ -44,10 +47,6 @@ keys_binds = [
     Key([super, alt], "c", lazy.spawn('code')),
     Key([super, alt], "d", lazy.spawn('discord')),
     Key([super], "Return", lazy.spawn(default_terminal)),
-
-    # tech
-    Key([super, "shift"], "r", lazy.spawn('bash -c "killall polybar; polybar leftbar & polybar rightbar &"')), # restart polybar
-    Key([super, "control"], "r", lazy.reload_config()), # restart qtile config
 ]
 
 # super + 1-9 --> switch group
@@ -57,17 +56,20 @@ for group in groups:
             Key([super], group.name, lazy.group[group.name].toscreen()),
 
             # super + alt + 1-9 = switch group and move focused window to group
-            Key([super, alt], group.name, lazy.window.togroup(group.name, switch_group=True)),
-            
+            Key([super, alt], group.name, lazy.window.togroup(
+                group.name, switch_group=True)),
+
             # or use this one if you prefer not to change the group when moving the window
-            
+
             # Key([super, alt], group.name, lazy.window.togroup(group.name)),
         ]
     )
 
 drag_binds = [
-    Drag([super], left_click, lazy.window.set_position_floating(), start=lazy.window.get_position()),
-    Drag([super], right_click, lazy.window.set_size_floating(), start=lazy.window.get_size()),
+    Drag([super], left_click, lazy.window.set_position_floating(),
+         start=lazy.window.get_position()),
+    Drag([super], right_click, lazy.window.set_size_floating(),
+         start=lazy.window.get_size()),
 ]
 
 click_binds = [
